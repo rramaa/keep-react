7127758e5b0cf3a31d945f66b1a488a57a483284
Ramakrishna <ramakrishna@proptiger.com>
2017-12-17 17:45:51 +0530

    prod-build

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,11 @@ var webpack = require('webpack')
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-var BUILD_DIR = path.resolve(__dirname, 'dist')
-var APP_DIR = path.resolve(__dirname, 'src/client')
-
-var config = {
-	entry: APP_DIR + '/index.jsx',
-	module: {
-		rules: [{
-			test: /\.html$/,
-			use: ["html-loader"]
-		}, {
-			test : /\.jsx?/,
-			include : APP_DIR,
-			loader : 'babel-loader'
-		}, {
-			test : /\.scss$/,
-			include : APP_DIR,
-			loader : ['style-loader', 'css-loader', 'sass-loader']
-		}]
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.scss', '.css'],
-		modules: ['node_modules', APP_DIR]
-	},
-	output: {
-		path: BUILD_DIR,
-		filename: 'app.js'
-	},
-	plugins: [new HtmlWebpackPlugin({
-		title: "React App",
-		template: APP_DIR + "/index.html"
-	})]
+var config
+if(process.env.NODE_ENV === 'production'){
+	config = require('./webpack/prod')(__dirname)
+} else {
+	config = require('./webpack/dev')(__dirname)
 }
 
 module.exports = config
